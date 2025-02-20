70bbaf7dc224223d65918e7c43bd90bee7bc1e7f
bzunzarren <zunzarren2014@gmail.com>
2025-02-20 15:00:41 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/PortifolioBruna/',
+  base: '/PortfolioBruna/', // Coloque o nome do seu repositório aqui
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
